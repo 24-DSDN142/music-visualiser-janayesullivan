c8b2e60787501c4b0a72846a77fbbcc9dce34d45
janayesullivan <sullivjana@myvuw.ac.nz>
2024-09-09 22:01:38 +1200

    changed the background colour and orientation

--- music_starter.js
+++ music_starter.js
@@ -8,100 +8,28 @@ let y = 120;
 
 
 var sizegrow = 90; //disco ball size
-var waveheight = 440; //movemet of waves up and down 
-var waveheighttwo = 540 // second set of waves movement
-var waveheightthree = 640 // third set of waves movement
+var waveheight = 200; //movemet of waves up and down 
+var waveheighttwo = 150 // second set of waves movement
+var waveheightthree = 100 // third set of waves movement
 
 let fishSize = 40
-let fishX = 200
-let fishY = 200
+let fishX = 245
+let fishY = 275
 
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-background(186, 210, 232)
-
-//lights
-fill(235, 240, 242, 80)
-noStroke();
-triangle (50, 50, 50, 50, 50, 50);
-
-
-//fish 
-DrawFish(fishX, fishY)
-
-
-function DrawFish (fishX, fishY) {
-fill(255)
-noStroke();
-triangle(fishX+45, fishY+75, fishX+85, fishY+35, fishX+85, fishY+115)
-triangle(fishX+85, fishY+75, fishX+100, fishX+60, fishX+100, fishY+90)
-
-}
+background(0, 107, 165)
 
 
 
-//wave set one 
-
-//wave base
-fill(218, 235, 245) // base of sound wave colour
+//wave base 3
+fill(85, 226, 233) // base of sound wave colour
 noStroke(); 
-rect(0,450,550,100);//base of sound wave
+rect(0,0,550,100);//base of sound wave
 
 
-//waves circles 
-fill(186, 210, 232) // colour of hidden wave circles (must match background)
-noStroke ();
-ellipse(565, waveheight, 90, 70);//wave far right
-ellipse(475, waveheight, 90, 70);//wave three right
-ellipse(385, waveheight, 90, 70);//wave two right
-ellipse(295, waveheight, 90, 70);//wave one right
-ellipse(205, waveheight, 90, 70);//wave middle
-ellipse(115, waveheight, 90, 70);//wave one left
-ellipse(25, waveheight, 90, 70);//wave two left 
-ellipse(-65, waveheight, 90, 70);//far left
-
-waveheight = waveheight + 0.42; //speed of height growth
-
-if(waveheight > 450){
-waveheight = 440;
-
-}
-
-//wave set two
-
-//wave base
-fill(174, 209, 230) // base of sound wave colour
-noStroke(); 
-rect(0,520,550,100);//base of sound wave
-
-//waves circles 
-fill(218, 235, 245) // colour of hidden wave circles (must match background)
-noStroke ();
-ellipse(520, waveheighttwo, 90, 70);//wave far right
-ellipse(430, waveheighttwo, 90, 70);//wave two right
-ellipse(340, waveheighttwo, 90, 70);//wave one right
-ellipse(250, waveheighttwo, 90, 70);//wave middle
-ellipse(160, waveheighttwo, 90, 70);//wave one left
-ellipse(70, waveheighttwo, 90, 70);//wave two left 
-ellipse(-20, waveheighttwo, 90, 70);//far left
-
-waveheighttwo = waveheighttwo + 0.28; //speed of height growth
-
-if(waveheighttwo > 520){
-waveheighttwo = 510;
-
-
-}
-
-//wave set there
-
-//wave base
-fill(114, 156, 181) // base of sound wave colour
-noStroke(); 
-rect(0,590,550,100);//base of sound wave
-
-//waves circles 
-fill(174, 209, 230) // colour of hidden wave circles (must match background)
+//waves circles  middle blue circles 
+fill(4, 186, 222) // colour of hidden wave circles (must match background)
 noStroke ();
 ellipse(565, waveheightthree, 90, 70);//wave far right
 ellipse(475, waveheightthree, 90, 70);//wave three right
@@ -111,34 +39,58 @@ ellipse(205, waveheightthree, 90, 70);//wave middle
 ellipse(115, waveheightthree, 90, 70);//wave one left
 ellipse(25, waveheightthree, 90, 70);//wave two left 
 ellipse(-65, waveheightthree, 90, 70);//far left
-
-waveheightthree = waveheightthree + 0.14; //speed of height growth
-
-if(waveheightthree > 590){
-waveheightthree = 580;
-
-}
-
-//flowers 
-fill(275)
-ellipse(120, 110, sizegrow, sizegrow); // ball
-ellipse(420, 110, sizegrow, sizegrow);
-
-
-sizegrow = sizegrow + 0.3; //speed of movement
-
-if(sizegrow > 100){ //loop (no bigger than ())
-sizegrow = 90; //loop restarts at ())
-
+waveheightthree = waveheightthree + 0.4; //speed of height growth
+if(waveheightthree > 100){
+waveheightthree = 90;
 }
 
 
+//wave base 2
+fill(4, 186, 222) // base of sound wave colour
+noStroke(); 
+rect(0,98,550,100);//base of sound wave
+
+//wave set two
+//waves circles light blue circles
+fill(4, 150, 199) // colour of hidden wave circles (must match background)
+noStroke ();
+ellipse(520, waveheighttwo, 90, 70);//wave far right
+ellipse(430, waveheighttwo, 90, 70);//wave two right
+ellipse(340, waveheighttwo, 90, 70);//wave one right
+ellipse(250, waveheighttwo, 90, 70);//wave middle
+ellipse(160, waveheighttwo, 90, 70);//wave one left
+ellipse(70, waveheighttwo, 90, 70);//wave two left 
+ellipse(-20, waveheighttwo, 90, 70);//far left
+waveheighttwo = waveheighttwo + 0.4; //speed of height growth
+if(waveheighttwo > 150){
+waveheighttwo = 140;
+}
+
+//wave base 1
+fill(4, 150, 199) // base of sound wave colour
+noStroke(); 
+rect(0,150,550,50);//base of sound wave
+//wave set one background blue circles
+//waves circles 
+fill(0, 107, 165) // colour of hidden wave circles (must match background)
+noStroke ();
+ellipse(565, waveheight, 90, 70);//wave far right
+ellipse(475, waveheight, 90, 70);//wave three right
+ellipse(385, waveheight, 90, 70);//wave two right
+ellipse(295, waveheight, 90, 70);//wave one right
+ellipse(205, waveheight, 90, 70);//wave middle
+ellipse(115, waveheight, 90, 70);//wave one left
+ellipse(25, waveheight, 90, 70);//wave two left 
+ellipse(-65, waveheight, 90, 70);//far left
+waveheight = waveheight + 0.4; //speed of height growth
+if(waveheight > 200){
+waveheight = 190;
+}
 
 
 
 
 }
-
 
 
 
